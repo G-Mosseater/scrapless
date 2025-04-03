86f7e515db12bb381a0b938b5aa369cc14c6344a
aythami <aythami.1914@gmail.com>
2025-04-03 08:43:23 +0100

    need to finish card

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -3,18 +3,16 @@ import Image from "next/image";
 export default function ProductCard() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center">
-                <div className="flex w-[130px] h-[160px] border border-[#14213D] rounded-t-2xl justify-center items-center">
-                    <Image
-                        src="/images/foto.png"
-                        width={130}
-                        height={160}
-                        alt="foto"
-                    />
-                </div>
-                <div className="flex justify-center w-[130px] h-[30px] border border-[#14213D] rounded-b-2xl">
-                    <p>name</p>
-                </div>
+            <div className="flex w-[130px] h-[160px] border border-[#14213D] rounded-t-2xl justify-center items-center">
+                <Image
+                    src="/images/foto.png"
+                    width={130}
+                    height={160}
+                    alt="foto"
+                />
+            </div>
+            <div className="flex justify-center w-[130px] h-[30px] border border-[#14213D] rounded-b-2xl">
+                <p>name</p>
             </div>
         </>
     )
