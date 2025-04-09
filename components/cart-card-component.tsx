a24acfe55bee9b87e9d50d5856cecf8a0d7f45d7
aythami <aythami.1914@gmail.com>
2025-04-09 13:24:55 +0100

    fix my box list component

--- components/cart-card-component.tsx
+++ components/cart-card-component.tsx
@@ -36,7 +36,7 @@ export default function CartCardComponent({ item, onRefresh }: Props) {
 
   return (
     <Card className="rounded-3xl border-[hsl(var(--blues))] w-full ">
-      <CardContent className="flex items-center p-4">
+      <CardContent className="flex items-center p-4 w-full">
         <div className="w-24 h-24 overflow-hidden rounded-md">
           <Image
             src={item.products.image_url || "/images/template-image.png"}
